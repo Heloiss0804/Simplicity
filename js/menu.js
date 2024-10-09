fa272ddf01a4cb83429d8382d92bf075589d08ea
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2024-10-09 11:40:21 -0300

    criando a partedo JS abrir/fechar

---
+++ js/menu.js
@@ -0,0 +1,24 @@
+// Selecionando o link com a palavrea menu
+const botaoMenu = document.querySelector(".titulo-menu a");
+
+// Sselecionando a lista de links menu
+const listaDeLinks = document.querySelector(".links-menu");
+
+// Ouvinte de Evento para o click no botão menu
+botaoMenu.addEventListener("click", function(event){
+    // Anulando o comportamneto padrão de recarregar a página através de um link.
+    event.preventDefault();
+
+    // abrimos ou fechamos a lista de links, usando toggle na classe aberto
+    listaDeLinks.classList.toggle("aberto");
+
+    if (listaDeLinks.classList.contains("aberto")){
+        botaoMenu.innerHTML = "Fechar &times";
+
+    }else {
+        botaoMenu.innerHTML = "menu &equiv"
+    }
+
+  
+    
+})
