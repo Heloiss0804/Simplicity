952e09dd7d458558e8f4dea290f34a2a23382487
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2024-10-15 08:12:04 -0300

    incluindo js na pagina contato

--- js/menu.js
+++ js/menu.js
@@ -1,24 +1,24 @@
-// Selecionando o link com a palavrea menu
+// Selecionando o link com a palavra Menu
 const botaoMenu = document.querySelector(".titulo-menu a");
 
-// Sselecionando a lista de links menu
+// Selecionando a lista de links do menu
 const listaDeLinks = document.querySelector(".links-menu");
 
 // Ouvinte de Evento para o click no botão menu
-botaoMenu.addEventListener("click", function(event){
-    // Anulando o comportamneto padrão de recarregar a página através de um link.
+botaoMenu.addEventListener("click", function( event ){
+    /* Anulando o comportamento padrão de recarregar a 
+    página através de um link */
     event.preventDefault();
 
-    // abrimos ou fechamos a lista de links, usando toggle na classe aberto
+    /* Abrimos/Fechamos a lista de links usando o toggle na classe aberto */
     listaDeLinks.classList.toggle("aberto");
 
-    if (listaDeLinks.classList.contains("aberto")){
+    if(listaDeLinks.classList.contains("aberto")){
         botaoMenu.innerHTML = "Fechar &times;";
-
-    }else {
-        botaoMenu.innerHTML = "menu &equiv;"
+    } else {
+        botaoMenu.innerHTML = "Menu &equiv;";
     }
+});
 
-  
-    
-})
+
+
