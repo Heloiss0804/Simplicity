7e18eddd4079092a4a04780b1adeb573188e81dc
Heloiss0804 <heloisa.germanobussi@yahoo.com.br>
2024-10-16 11:25:35 -0300

    Ativando a página contato com formspree

--- js/contato.js
+++ js/contato.js
@@ -82,3 +82,38 @@ botaoBuscar.addEventListener("click", async function () {
     }
 }); // final do evento do botão
 
+
+
+
+// Código do Formspree
+
+var form = document.getElementById("my-form");
+  
+  async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+          'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Obrigado!!Mensagem enviada";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! Deu erro, tente de novo"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! Houve um erro, verifique com administrador"
+    });
+  }
+  form.addEventListener("submit", handleSubmit)
